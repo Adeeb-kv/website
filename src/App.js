e2ffdfc1a933f5833af1ea66271d58b6bad46ce9
Adeeb-kv <adeebirfan56@gmail.com>
2024-04-30 12:15:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import react from "react";
+import "./App.css";
+
+function App(){
+  return <div><center><div className="header"><h1>ABOUT ME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MY WORK&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CONTACT</h1></div>
+  <div className="h2"><h2>I&nbsp;&nbsp;&nbsp;&nbsp;SAW&nbsp;&nbsp;&nbsp;&nbsp;THE&nbsp;&nbsp;&nbsp;&nbsp;SIGN</h2></div>
+  <div className="paragraph"><p>and i opened up my eyes.One of my favourite things to do is to capture the american nostalgiaof signage.They tell us
+    where we're going or where we've have been.They express a mood or saying can even just be a shape.But it's the vintage,
+    neon,and big light bulbed signs thet say so much more.They are'nt just your average square shapes with letters on it.
+    Some actual thought and planning went into the making of the sign.Oh and the lost are of advertising with neon signs
+    that display motion is always a fun find! Such a lost art in my opinion.Led just my eyes..does'nt it?
+  </p></div>
+  <img className="imgmain" src="https://tse2.mm.bing.net/th?id=OIP.70Fhg0DKkqVGHORdmI1UiQHaHa&pid=Api&P=0&h=180"></img>
+  <div className="imagr1">
+  <div className="column"><img className="bacon"src="https://images.hdqwalls.com/download/long-live-bacon-neon-lights-s9-1920x1440.jpg"></img><p>Neon on brick</p></div>
+  <div className="column"><img className="bacon"src="https://tse3.mm.bing.net/th?id=OIP.zK7uYCSJiz29HuLz9yTragHaEK&pid=Api&P=0&h=180"></img><p>Classic enter sign</p></div>
+  <div className="column"><img className="bacon"src="https://images.unsplash.com/photo-1550496635-97c10f749275?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW15fGVufDB8fDB8fHww"></img><p>Message in a garage</p></div>
+  </div>
+  <div className="imagr2">
+  <div className="column"><img className="bacon"src="https://thumbs.dreamstime.com/b/which-way-to-go-signpost-184689920.jpg"></img><p>which way image</p></div>
+  <div className="column"><img className="bacon"src="https://tse3.mm.bing.net/th?id=OIP.zK7uYCSJiz29HuLz9yTragHaEK&pid=Api&P=0&h=180"></img><p>Classic enter sign</p></div>
+  <div className="column"><img className="bacon"src="https://images.unsplash.com/photo-1550496635-97c10f749275?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW15fGVufDB8fDB8fHww"></img><p>Message in a garage</p></div>
+  </div>
+  </center></div>
+}
+export default App
